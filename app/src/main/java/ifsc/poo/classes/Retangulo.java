217625a3bdacdebacc226d8c6736023d8f195a18
ArthurOReis <arthuroreis2005@gmail.com>
2025-04-10 11:19:48 -0300

    Implementando atributos e métodos

--- app/src/main/java/ifsc/poo/classes/Retangulo.java
+++ app/src/main/java/ifsc/poo/classes/Retangulo.java
@@ -1,5 +1,30 @@
 package ifsc.poo.classes;
 
 public class Retangulo {
-    
+    private float largura;
+    private float altura;
+
+    public float getLargura(){
+        return this.largura;
+    }
+
+    public void setLargura(float largura){
+        this.largura = largura;
+    }
+
+    public float getAltura(){
+        return this.altura;
+    }
+
+    public void setAltura(float altura){
+        this.altura = altura;
+    }
+
+    public float getArea(){
+        return this.largura * this.altura;
+    }
+
+    public float getPerimetro(){
+        return 2*(this.largura + this.altura);
+    }
 }
